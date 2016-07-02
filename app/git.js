c6b333fb6603131ac1c4d3440c7683805f2f6ba7
Julian <jdtorregrosas@gmail.com>
2016-07-02 19:25:52 +0200

    Add support to log from one tag to another
    Add functions' documentation

--- app/git.js
+++ app/git.js
@@ -6,24 +6,39 @@ const fse = promisify(require('fs-extra'))
 const temporalPath = `${__dirname}/../tmp`
 
 module.exports = {
+  /**
+   * Clone a repository's specific branch in the temporal path
+   * @param {string} url
+   * @param {string} branch
+   * @param {function} callback
+   */
   clone: function (url, branch = 'master', callback) {
     const command = `git clone -b ${branch} ${url} ${temporalPath}`
     fse.remove(temporalPath).then(function () {
       exec(command, { cwd: `${__dirname}/../` }, function (err, stdout, stderr) {
-        // if (err) callback(err)
+        // TODO: Error
         callback(stdout)
       })
     })
   },
-  log: function (latestTag = '', callback) {
+  /**
+   * Log in a String all the commit messages in a range
+   * @param {Object} options{endTag: {string}, startTag: {string}}
+   * @param {function} callback
+   */
+  log: function (options = {}, callback) {
     let command
-    if (latestTag === '') {
+    if (!options.endTag) {
       command = `git log --oneline --format="_TITLE%s%n %b"`
+    } else if (!options.startTag && options.endTag) {
+      command = `git log ${options.endTag}..HEAD --oneline --format="_TITLE%s%n %b"`
+    } else if (options.startTag && options.endTag) {
+      command = `git log ${options.endTag}..${options.startTag} --oneline --format="_TITLE%s%n %b"`
     } else {
-      command = `git log ${latestTag}..HEAD --oneline --format="_TITLE%s%n %b"`
+      // TODO: Throw error
     }
     exec(command, { cwd: temporalPath }, function (err, stdout, stderr) {
-      // if (err) callback(err)
+      // TODO: Error
       callback(stdout)
     })
   }
