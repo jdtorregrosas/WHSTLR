54ee6c09427906c1b13f654c42d6afe886a306b3
Julian <jdtorregrosas@gmail.com>
2016-07-02 18:04:46 +0200

    Add support to print all the history when no tag is given
    Fix .git ignore excluding release's builds

--- git.js
+++ git.js
@@ -9,15 +9,20 @@ module.exports = {
     const command = `git clone -b ${branch} ${url} ./tmp`
     fse.remove('./tmp').then(function () {
       exec(command, { cwd: './' }, function (err, stdout, stderr) {
-        if (err) callback(err)
+        // if (err) callback(err)
         callback(stdout)
       })
     })
   },
-  log: function (latestTag, callback) {
-    const command = `git log ${latestTag}..HEAD --no-color --oneline --format="_TITLE%s%n %b"`
+  log: function (latestTag = '', callback) {
+    let command
+    if (latestTag === '') {
+      command = `git log --oneline --format="_TITLE%s%n %b"`
+    } else {
+      command = `git log ${latestTag}..HEAD --oneline --format="_TITLE%s%n %b"`
+    }
     exec(command, { cwd: './tmp' }, function (err, stdout, stderr) {
-      if (err) callback(err)
+      // if (err) callback(err)
       callback(stdout)
     })
   }
