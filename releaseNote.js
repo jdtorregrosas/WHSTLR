719aaea8aec5d7da5d42bb19047f8055e52386a5
Julian <jdtorregrosas@gmail.com>
2016-07-02 17:35:44 +0200

    Create releaseNote class
    Create a file
    
    Add support to write direct in the file
    
    Add initial data to the file

---
+++ releaseNote.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const fs = require('fs')
+
+function ReleaseNote (repositoryName, releaseVersion) {
+  this.releaseFile = `./releases/${repositoryName}_${releaseVersion}.md`
+  fs.writeFileSync(this.releaseFile, `# ${repositoryName}\n*****\n`)
+  this.addTitle(releaseVersion, 2)
+}
+
+ReleaseNote.prototype.addItem = function (message) {
+  fs.appendFileSync(this.releaseFile, `* ${message}\n`)
+}
+
+ReleaseNote.prototype.addSubItem = function (message) {
+  fs.appendFileSync(this.releaseFile, ` * ${message}\n`)
+}
+
+ReleaseNote.prototype.addTitle = function (title, size = 3) {
+  let h = '### '
+  let end = '\n'
+  if (size === 1) {
+    h = '# '
+    end = '\n*****\n'
+  } else if (size === 2) {
+    h = '## '
+    end = '\n'
+  } else if (size === 3) {
+    h = '### '
+    end = ':\n'
+  }
+  fs.appendFileSync(this.releaseFile, `${h}${title}${end}`)
+}
+
+module.exports = ReleaseNote
