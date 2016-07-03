c4ef05f31d475eb9d739552abb670554828bcb42
Julian <jdtorregrosas@gmail.com>
2016-07-03 15:53:20 +0200

    add about route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const path = require('path')
 const healthRoute = require('./routes/_health')
 const indexRoute = require('./routes/index')
+const aboutRoute = require('./routes/about')
 const bodyParser = require('body-parser')
 const app = express()
 
@@ -16,8 +17,8 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-console.log(__dirname + '/releases')
 app.use('/releases', express.static(__dirname + '/releases'))
 app.use('/public', express.static(__dirname + '/public'))
 app.use('/', healthRoute)
 app.use('/', indexRoute)
+app.use('/', aboutRoute)
