b89427dd88f8e5f8320459767916bd27f7fa643e
Julian <jdtorregrosas@gmail.com>
2016-07-03 15:54:20 +0200

    Clean message variables after render

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,19 @@
 const express = require('express')
 const app = express()
 const execute = require('../execute').execute
-// const repositoryURL = 'https://github.com/jdtorregrosas/releaseNotesPrinter.git'
-const md = require('node-markdown').Markdown
+// const md = require('node-markdown').Markdown
 const branch = 'master'
 let successMessage
 let error
 
-/* GET Hello World page. */
+/* GET Index. */
 app.get('/index', (req, res) => {
   res.render('index', {
-    md: md,
-    title: 'Release Notes Printer',
-    subtitle: '',
     successMessage: successMessage,
     error
   })
+  successMessage = ''
+  error = ''
 })
 
 app.post('/createNotes', (req, res) => {
