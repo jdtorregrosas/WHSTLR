daeecf5b5d08ededf26a839c4d740211d701f4c5
Julian <jdtorregrosas@gmail.com>
2016-07-13 13:06:56 +0100

    Add max items per page and fix resolve in promises

--- app/gitlab/GitlabClient.js
+++ app/gitlab/GitlabClient.js
@@ -5,11 +5,12 @@ const request = require('superagent')
 function GitlabClient (baseURL, token) {
   this.baseURL = baseURL
   this.token = token
+  this.maxItems = 1000
 }
-//TODO test tags
+// TODO test tags
 GitlabClient.prototype.getTags = function (projectId) {
   return new Promise((resolve, reject) => {
-    const url = `${this.baseURL}/api/v3/projects/${projectId}/repository/tags`
+    const url = `${this.baseURL}/api/v3/projects/${projectId}/repository/tags?per_page=${this.maxItems}`
     request
     .get(url)
     .set('PRIVATE-TOKEN', this.token)
@@ -21,7 +22,7 @@ GitlabClient.prototype.getTags = function (projectId) {
 }
 GitlabClient.prototype.getProjects = function () {
   return new Promise((resolve, reject) => {
-    const url = `${this.baseURL}/api/v3/projects`
+    const url = `${this.baseURL}/api/v3/projects?per_page=${this.maxItems}`
     request
     .get(url)
     .set('PRIVATE-TOKEN', this.token)
@@ -30,40 +31,40 @@ GitlabClient.prototype.getProjects = function () {
       let projects = []
       for (let project in res.body) {
         projects[project] = res.body[project]
-        resolve(projects)
       }
+      resolve(projects)
     })
   })
 }
 GitlabClient.prototype.getCommits = function (projectId) {
   return new Promise((resolve, reject) => {
-    const url = `${this.baseURL}/api/v3/projects/${projectId}/repository/commits`
+    const url = `${this.baseURL}/api/v3/projects/${projectId}/repository/commits?per_page=${this.maxItems}`
     request
     .get(url)
     .set('PRIVATE-TOKEN', this.token)
     .end((err, res) => {
       if (err) reject(err)
-      let projects = []
-      for (let project in res.body) {
-        projects[project] = res.body[project]
-        resolve(projects)
+      let commits = []
+      for (let commit in res.body) {
+        commits[commit] = res.body[commit]
       }
+      resolve(commits)
     })
   })
 }
 GitlabClient.prototype.getMergeRequests = function (projectId) {
   return new Promise((resolve, reject) => {
-    const url = `${this.baseURL}/api/v3/projects/${projectId}/merge_requests?state=merged`
+    const url = `${this.baseURL}/api/v3/projects/${projectId}/merge_requests?state=merged?per_page=${this.maxItems}`
     request
     .get(url)
     .set('PRIVATE-TOKEN', this.token)
     .end((err, res) => {
       if (err) reject(err)
-      let projects = []
-      for (let project in res.body) {
-        projects[project] = res.body[project]
-        resolve(projects)
+      let mergeRequests = []
+      for (let mergeRequest in res.body) {
+        mergeRequests[mergeRequest] = res.body[mergeRequest]
       }
+      resolve(mergeRequests)
     })
   })
 }
