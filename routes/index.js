0f8e5daa6cbec5e185cf7135cd78b49eafae1db2
Julian <jdtorregrosas@gmail.com>
2016-07-13 13:05:44 +0100

    Implement gitlab client use

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@
 
 const express = require('express')
 const app = express()
-const execute = require('../execute').execute
+const GitlabClient = require('../app/gitlab/GitlabClient')
+const converter = require('../app/converter')
+// const mock = require('../test/APIMock.js')
+// const execute = require('../execute').execute
 // const md = require('node-markdown').Markdown
-const branch = 'master'
-const mock = require('../test/APIMock.js')
+// const branch = 'master'
 let successMessage
 let user, projects
-let tags
 let commits
 let mergeRequests
+let tags
 let error
 // let error
+let gitlabClient
 
 app.get('/', (req, res) => {
   res.redirect('/config')
@@ -27,7 +30,6 @@ app.get('/index', (req, res) => {
     res.render('index', {
       user: user,
       projects: projects,
-      tags: tags,
       commits: commits,
       mergeRequests: mergeRequests,
       error: error,
@@ -40,31 +42,63 @@ app.get('/index', (req, res) => {
 })
 
 app.post('/applyConfig', (req, res) => {
-  user = mock.getUser()
-  projects = mock.getProjects()
-  res.redirect('/index')
+  let token = req.body.token
+  let baseURL = req.body.baseURL
+  gitlabClient = new GitlabClient(baseURL, token)
+  user = gitlabClient.getCurrentUser().then((user) => {
+    gitlabClient.getProjects().then((res) => {
+      projects = converter.convertProjects(res)
+    }).then(() => {
+      res.redirect('/index')
+    }).catch((err) => {
+      error = {title: 'Error', description: err}
+    })
+  })
 })
 
 app.post('/getTags', (req, res) => {
-  projects = mock.getProjects()
   for (let project in projects) {
     if (projects[project].name === req.body.project) {
-      let tags = mock.getTags(projects[project].id)
-      res.send(tags)
+      gitlabClient.getTags(projects[project].id)
+      .then((res) => {
+        tags = converter.convertTags(res.body)
+      }).then(() => {
+        res.send(tags)
+      }).catch((err) => {
+        error = {title: 'Error', description: err}
+      })
     }
   }
 })
 
 app.post('/createNotes', (req, res) => {
-  const repositoryURL = req.body.repository
-  const version = req.body.newVersion
-  const logOptions = {
-    endTag: req.body.endVersion
+  commits = []
+  mergeRequests = []
+  console.log(tags)
+  for (let project in projects) {
+    if (projects[project].name === req.body.projects) {
+      gitlabClient.getCommits(projects[project].id)
+      .then((res) => {
+        commits = converter.convertCommits(res)
+      }).then(() => {
+        gitlabClient.getMergeRequests(projects[project].id).then((result) => {
+          mergeRequests = converter.convertMergeRequests(result)
+        }).then(() => {
+          res.redirect('/index')
+        }).catch((err) => {
+          error = {title: 'Error', description: err}
+          commits = []
+          mergeRequests = []
+          res.redirect('/index')
+        })
+      }).catch((err) => {
+        error = {title: 'Error', description: err}
+        commits = []
+        mergeRequests = []
+        res.redirect('/index')
+      })
+    }
   }
-  commits = mock.getCommits()
-  mergeRequests = mock.getMergeRequests()
-  error = mock.getError()
-  res.redirect('/index')
 })
 
 module.exports = app
