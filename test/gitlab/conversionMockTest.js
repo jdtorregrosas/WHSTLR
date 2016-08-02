a849f467203bce60af4e79a163a75c227c088deb
Julian <jdtorregrosas@gmail.com>
2016-08-02 09:45:09 +0100

    Improve tests

---
+++ test/gitlab/conversionMockTest.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const mock = require('./mockServer.js')
+const converter = require('../../app/gitlab/gitlabConvert.js')
+const projectsSchema = require('../../app/schemas/projectsSchema.js')
+const commitsSchema = require('../../app/schemas/commitsSchema.js')
+const tagsSchema = require('../../app/schemas/tagsSchema.js')
+const mergeRequestsSchema = require('../../app/schemas/mergeRequestsSchema.js')
+const request = require('supertest')
+
+describe('# Projects', () => {
+  it('should output a datastructure as specified in the schema', (done) => {
+    request(mock)
+    .get('/api/v3/projects')
+    .end((err, res) => {
+      if (err) done(err)
+      else {
+        const projects = converter.convertProjects(res.body)
+        projectsSchema.validateProjects(projects)
+        done()
+      }
+    })
+  })
+})
+
+describe('# Commits', () => {
+  it('should output a datastructure as specified in the schema', (done) => {
+    request(mock)
+    .get('/api/v3/projects/140/repository/commits')
+    .end((err, res) => {
+      if (err) done(err)
+      else {
+        const commits = converter.convertCommits(res.body)
+        commitsSchema.validateCommits(commits)
+        done()
+      }
+    })
+  })
+})
+
+describe('# Merge Requests', () => {
+  it('should output a datastructure as specified in the schema', (done) => {
+    request(mock)
+    .get('/api/v3/projects/140/merge_requests')
+    .end((err, res) => {
+      if (err) done(err)
+      else {
+        const mergeRequests = converter.convertMergeRequests(res.body)
+        mergeRequestsSchema.validateMergeRequests(mergeRequests)
+        done()
+      }
+    })
+  })
+})
+
+describe('# Tags', () => {
+  it('should output a datastructure as specified in the schema', (done) => {
+    request(mock)
+    .get('/api/v3/projects/140/repository/tags')
+    .end((err, res) => {
+      if (err) done(err)
+      else {
+        const tags = converter.convertTags(res.body)
+        tagsSchema.validateTags(tags)
+        done()
+      }
+    })
+  })
+})
--- test/gitlab/convertTest.js
+++ test/gitlab/convertTest.js
@@ -70,3 +70,91 @@ describe('# Commits', () => {
     }, 'Expected error')
   })
 })
+
+describe('# Merges', () => {
+  it('Should throw error when mergeRequests without message are comming', () => {
+    assert.throws(() => {
+      const badMergeRequests = [{
+        test: 'test'
+      }, {
+        test2: 'test2'
+      }]
+      converter.convertMergeRequests(badMergeRequests)
+    }, TypeError, 'Expected error')
+  })
+  it('Should throw error when mergeRequests are not an array', () => {
+    assert.throws(() => {
+      const badMergeRequests = 'thatSNotAnArray'
+      converter.convertMergeRequests(badMergeRequests)
+    }, TypeError, 'Expected error')
+  })
+  it('Should throw error when mergeRequests are not an array of objects', () => {
+    assert.throws(() => {
+      const badMergeRequests = [1, 2, 3, 4, 5]
+      converter.convertMergeRequests(badMergeRequests)
+    }, TypeError, 'Expected error')
+  })
+
+  it('Should convert a mergeRequest to the expected schema', () => {
+    const goodMergeRequests = [{
+      'id': 1456,
+      'project_id': 140,
+      'title': 'Regiestrierungsdatum f575',
+      'description': 'Add createdAt in the response',
+      'state': 'merged',
+      'created_at': '2016-06-30T10:39:46.649+02:00',
+      'updated_at': '2016-07-07T11:40:50.742+02:00',
+      'target_branch': 'master',
+      'source_branch': 'regiestrierungsdatum-f575',
+      'author': {
+        'name': 'Julian David Torregrosa Simbaqueva',
+        'username': 'jdtorregrosas',
+        'id': 30,
+        'state': 'active',
+        'avatar_url': 'http://gitlab.local.coliquio.de/uploads/user/avatar/30/poochieAvatar.png',
+        'web_url': 'http://gitlab.local.coliquio.de/u/jdtorregrosas'
+      }
+    }, {
+      'id': 1453,
+      'project_id': 140,
+      'title': 'Regiestrierungsdatum f572',
+      'description': 'Add createdAt in the response 2',
+      'state': 'merged',
+      'created_at': '2016-06-30T10:39:46.649+02:00',
+      'updated_at': '2016-07-07T11:40:50.742+02:00',
+      'target_branch': 'master',
+      'source_branch': 'regiestrierungsdatum-f5752',
+      'author': {
+        'name': 'Julian David Torregrosa Simbaqueva',
+        'username': 'jdtorregrosas',
+        'id': 30,
+        'state': 'active',
+        'avatar_url': 'http://gitlab.local.coliquio.de/uploads/user/avatar/30/poochieAvatar.png',
+        'web_url': 'http://gitlab.local.coliquio.de/u/jdtorregrosas'
+      }
+    }]
+    const convertedMergeRequests = converter.convertMergeRequests(goodMergeRequests)
+    assert.doesNotThrow(() => {
+      mergeRequestsSchema.validateMergeRequests(convertedMergeRequests)
+    }, 'Invalid Schema')
+  })
+
+  it('Should throw error when the schema is wrong', () => {
+    const goodMergeRequests = [{
+      'id': '8574912c1880ce03f89d6d666d0c624c1521e4f0',
+      'title': 'Merge branch',
+      'author_name': 'Tobias Sailer',
+      'created_at': '2016-07-07T11:40:45.000+02:00',
+      'message': 'Update documentation descriptions 1'
+    }, {
+      'id': '6d9a3467990ac05de599c3c3757d342da4e1df3e',
+      'title': 'Update documentation descriptions',
+      'author_name': 'Julian',
+      'created_at': '2016-06-30T17:03:59.000+01:00',
+      'message': 'Update documentation descriptions 2'
+    }]
+    assert.throws(() => {
+      commitsSchema.validateMergeRequests(goodMergeRequests)
+    }, 'Expected error')
+  })
+})
