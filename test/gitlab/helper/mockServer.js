4e45644ffb7ab96abad0dec9ff1c4ebc71cf8625
Julian <jdtorregrosas@gmail.com>
2016-08-06 22:55:30 +0200

    Add helper folder with helper files

---
+++ test/gitlab/helper/mockServer.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+const projects = require('./mockData/projects.json')
+const commits = require('./mockData/commits140.json')
+const tags = require('./mockData/tags140.json')
+const mergeRequests = require('./mockData/mergeRequests140.json')
+const user = require('./mockData/user.json')
+
+app.get('/api/v3/projects', (req, res) => {
+  res.send(projects)
+})
+
+app.get('/api/v3/projects/140/repository/commits', (req, res) => {
+  res.send(commits)
+})
+
+app.get('/api/v3/projects/140/repository/tags', (req, res) => {
+  res.send(tags)
+})
+
+app.get('/api/v3/projects/140/merge_requests', (req, res) => {
+  res.send(mergeRequests)
+})
+
+app.get('/api/v3/projects/140/merge_requests/1456/commits', (req, res) => {
+  res.send(commits)
+})
+
+app.get('/api/v3/projects/140/merge_requests/1436/commits', (req, res) => {
+  res.send(commits)
+})
+app.get('/api/v3/projects/140/merge_requests/1404/commits', (req, res) => {
+  res.send(commits)
+})
+app.get('/api/v3/projects/140/merge_requests/1401/commits', (req, res) => {
+  res.send(commits)
+})
+
+app.get('/api/v3/user', (req, res) => {
+  res.send(user)
+})
+
+app.listen(2000, () => {
+  console.log('Mock server in port', 2000)
+})
+
+module.exports = app
