1ae2ea7c07fb36ad26282dc20e92c0d4c21d5633
Julian <jdtorregrosas@gmail.com>
2016-08-07 18:10:24 +0200

    Add more trailing slashes

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -2,7 +2,7 @@ function configLoading() {
   swal({
     title: "Wait a moment, please!",
     text: "We are applying your configuration ",
-    imageUrl: "public/assets/images/loading.png"
+    imageUrl: "/public/assets/images/loading.png"
   });
 }
 
@@ -10,7 +10,7 @@ function indexLoading() {
   swal({
     title: "Wait a moment, please!",
     text: "We are trying to proccess your request",
-    imageUrl: "public/assets/images/thumbs-up.jpg",
+    imageUrl: "/public/assets/images/thumbs-up.jpg",
     timer: 2000,
     showConfirmButton: false
   });
