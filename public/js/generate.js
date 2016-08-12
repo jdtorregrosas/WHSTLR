519bb11017cffe694d281479b1dde050c77f0eab
Julian <jdtorregrosas@gmail.com>
2016-08-13 01:47:18 +0200

    Push messages to a release note Add and remove

--- public/js/generate.js
+++ public/js/generate.js
@@ -2,3 +2,13 @@ $(document).on('click', '#btnGenerate', function() {
   var projectName = $('#projects option:selected').text()
   $('#releaseNoteTitle').text(projectName)
 })
+
+$(document).on('click', '[id^=check-]', function() {
+  var id = $(this).attr('id').replace(/check-/,'')
+  var message = $(`#commit-${id}`).text()
+  if($(this).is(':checked')){
+    $('#releaseNoteFeatures').append(`<li id="feature-${id}"> ${message} </li>`)
+  } else {
+    $(`#feature-${id}`).remove()
+  }
+})
