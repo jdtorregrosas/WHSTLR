f8015e5c89148670a00396b55deb24d4ad899403
Julian <jdtorregrosas@gmail.com>
2016-08-14 00:08:13 +0200

    Fix misstype

--- mocks.js
+++ mocks.js
@@ -4,4 +4,4 @@ const gitlabMock = require('./test/gitlab/helper/mockServer')
 const githubMock = require('./test/github/helper/mockServer')
 
 gitlabMock.listen(2001, () => {console.log('Running Gitlab mock in port 2001');})
-gitlabMock.listen(2002, () => {console.log('Running Github mock in port 2002');})
+githubMock.listen(2002, () => {console.log('Running Github mock in port 2002');})
