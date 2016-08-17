b6eeff06f0a1134f4fab427bae7cbe941ee9b4d9
Julian <jdtorregrosas@gmail.com>
2016-08-17 23:11:14 +0200

    Test deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-{
-  # clear any previous sudo permission
-    sudo -k
-
-    # run inside sudo
-    sudo sh <<SCRIPT
-    rm -fdr WHSTLR
-    git clone https://github.com/jdtorregrosas/WHSTLR.git
-    cd WHSTLR
-    git checkout master
-    git pull
-    rm -fdr node_modules/*
-    npm install
-    npm run bower
-    node app.js
-    echo "Now running WHSTLR in production"
-
-    SCRIPT
-}
+rm -fdr WHSTLR
+git clone https://github.com/jdtorregrosas/WHSTLR.git
+cd WHSTLR
+git checkout master
+git pull
+rm -fdr node_modules/*
+npm install --quiet --production
+npm run bower
+npm start
+echo "Now running WHSTLR in production"
